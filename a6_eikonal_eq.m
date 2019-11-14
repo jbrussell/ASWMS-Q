562034935abe893a76bb6ebc6440bd8e399442f5
jbrussell <jbrussell93@gmail.com>
2019-11-14 04:21:54 -0500

    update fields of 2D eikonal

--- a6_eikonal_eq.m
+++ a6_eikonal_eq.m
@@ -304,11 +304,14 @@ for ie = 1:length(csmatfiles)
 		eventphv(ip).id = eventcs.id;
 		eventphv(ip).evla = eventcs.evla;
 		eventphv(ip).evlo = eventcs.evlo;
+		eventphv(ip).evdp = eventcs.evdp;
 		eventphv(ip).period = periods(ip);
 		eventphv(ip).traveltime = travel_time(ip).tp;
 		eventphv(ip).stlas = eventcs.stlas;
 		eventphv(ip).stlos = eventcs.stlos;
 		eventphv(ip).stnms = eventcs.stnms;
+		eventphv(ip).isgood = w>0;
+		eventphv(ip).Mw = eventcs.Mw;
 		disp(['Period:',num2str(periods(ip)),', Goodnum:',num2str(eventphv(ip).goodnum),...
 				'Badnum:',num2str(eventphv(ip).badnum)]);
 	end % end of periods loop
