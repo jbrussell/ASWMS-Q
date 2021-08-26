9b18edffbda90fe8aff3db03a5d26dc4868c0945
jbrussell <jbrussell93@gmail.com>
2021-08-26 12:35:02 -0400

    add option for Love-wave excitation

--- functions_excitation/Love_GetExcitationRatios.m
+++ functions_excitation/Love_GetExcitationRatios.m
@@ -1,106 +1,110 @@
+
+function [eventcs] = Love_GetExcitationRatios(eventcs,Periodlist,CARD,Path2Phv,Path2Eig,MaxN,MinorOrMajor)
 %% Loops to Calculate Excitation ratios
 %% for a specific period, and for a set of events
 
+Depthlist = eventcs.evdp;
+Mrrlist = eventcs.moment.m_rr;
+Mttlist = eventcs.moment.m_tt;
+Mpplist = eventcs.moment.m_pp;
+Mrtlist = eventcs.moment.m_rt;
+Mrplist = eventcs.moment.m_rp;
+Mtplist = eventcs.moment.m_tp;
 
-% Loop over overtones
-for currN = [0:1:MaxN]
+% Loop over stations
+for ista = 1:length(eventcs.autocor)
+    [~, Azimuthlist] = distance(eventcs.evla,eventcs.evlo,...
+                                eventcs.stlas(ista),eventcs.stlos(ista)...
+                                ,referenceEllipsoid('GRS80'));
+    
+    % Loop over overtones
+    for currN = [0:1:MaxN]
 
-    qfile = dir([Path2Phv,'/',CARD,'.t*to*.q']);
-    qfile = [Path2Phv,'/',qfile(1).name];
-    mode = readMINEOS_qfile(qfile,currN);
-    Tlist=mode.T;
-    Clist=mode.phv;
-  
-    periodcounter = 0;
+        qfile = dir([Path2Phv,'/',CARD,'.t*to*.q']);
+        qfile = [Path2Phv,'/',qfile(1).name];
+        mode = readMINEOS_qfile(qfile,currN);
+        Tlist=mode.T;
+        Clist=mode.phv;
 
-    % Loop over periods
-    for period = Periodlist
-          Tdiff = abs(Tlist-period);
+        periodcounter = 0;
+
+        % Loop over periods
+        for period = Periodlist
+
+            Tdiff = abs(Tlist-period);
             [mindiff,cdx]=min(Tdiff);
             CurrC = Clist(cdx);
             CurrC = deg2rad(km2deg(CurrC));
-    
-  
-        Make_Filename
-        
-        periodcounter = periodcounter +1;
-        % Load eigenfunction
-        Eigfname=[Path2Eig,'/',CARD,'/',CARD,'.T',num2str(currN),'.mat'];
-        tmpinfo=load(Eigfname);
-        eig = tmpinfo.eig;
-        pers_eig = [eig(:).per_want];
-        [~,Iper] = min(abs(pers_eig-period));
-        U=eig(Iper).u;
-        Uderiv=eig(Iper).up;
-        V=eig(Iper).v;
-        Vderiv=eig(Iper).vp;
-        r=eig(1).r;  
-        
-        if currN > 0 & MinorOrMajor
-            wvgrpdx=2;
-        else
-            wvgrpdx=1;
-        end
-        
-        % Loop over events
-        for evnum = 1:1:length(Depthlist)
-            disp(['Percent Complete for n = ' num2str(currN) ' : ' num2str(100*evnum/length(Depthlist)) '%'])
-            
-            
-            if Azimuthlist(evnum) == 9999
 
-                [ B_SourceAmp,B_SourcePhase,B_Complex_Rad_Pattern,B_Term1,...
-                B_Term2 ] = ...
-                GetLoveSourceAmpandPhase([0:0.5:360],1000*Depthlist(evnum),period,...
-                r, W, Wderiv,Mrrlist(evnum),Mttlist(evnum),...
-                Mpplist(evnum),Mrtlist(evnum),Mrplist(evnum),...
-                Mtplist(evnum),CurrC,wvgrpdx );  
-            
-                tmpmax = max(B_SourceAmp); maxB_SourceAmp = tmpmax(1);
+            periodcounter = periodcounter +1;
+            % Load eigenfunction
+            Eigfname=[Path2Eig,'/',CARD,'.T',num2str(currN),'.mat'];
+            tmpinfo=load(Eigfname);
+            eig = tmpinfo.eig;
+            pers_eig = [eig(:).per_want];
+            [~,Iper] = min(abs(pers_eig-period));
+            U=eig(Iper).u;
+            Uderiv=eig(Iper).up;
+            V=eig(Iper).v;
+            Vderiv=eig(Iper).vp;
+            r=eig(1).r;    
 
-                PeriodStruc(periodcounter).RawExcitation_Mat(evnum,currN+1) = maxB_SourceAmp;                
-                
+            if currN > 0 & MinorOrMajor
+                wvgrpdx=2;
             else
-                            
-                [ B_SourceAmp,B_SourcePhase,B_Complex_Rad_Pattern,B_Term1,...
-                B_Term2 ] = ...
-                GetLoveSourceAmpandPhase(Azimuthlist(evnum),1000*Depthlist(evnum),period,...
-                r, W, Wderiv,Mrrlist(evnum),Mttlist(evnum),...
+                wvgrpdx=1;
+            end
+
+            % Loop over events
+            for evnum = 1:1:length(Depthlist)
+
+                 [ B_SourceAmp,B_SourcePhase,B_Complex_Rad_Pattern,B_Term1,...
+                B_Term2,B_Term3 ] = ...
+                GetLoveSourceAmpandPhase([0:0.5:360],1000*Depthlist(evnum),period,...
+                r, U, Uderiv,V,Vderiv,Mrrlist(evnum),Mttlist(evnum),...
                 Mpplist(evnum),Mrtlist(evnum),Mrplist(evnum),...
                 Mtplist(evnum),CurrC,wvgrpdx );  
 
-                PeriodStruc(periodcounter).RawExcitation_Mat(evnum,currN+1) = B_SourceAmp;
+                tmpmax = max(B_SourceAmp); maxB_SourceAmp = tmpmax(1);
+                PeriodStruc(periodcounter).period(evnum) = period;
+                PeriodStruc(periodcounter).mode(evnum,currN+1) = currN;
+                PeriodStruc(periodcounter).Amp_max(evnum,currN+1) = maxB_SourceAmp;
                 
+
+                [ B_SourceAmp,B_SourcePhase,B_Complex_Rad_Pattern,B_Term1,...
+                B_Term2,B_Term3 ] = ...
+                GetLoveSourceAmpandPhase(Azimuthlist(evnum),1000*Depthlist(evnum),period,...
+                r, U, Uderiv,V,Vderiv,Mrrlist(evnum),Mttlist(evnum),...
+                Mpplist(evnum),Mrtlist(evnum),Mrplist(evnum),...
+                Mtplist(evnum),CurrC,wvgrpdx );  
+
+                PeriodStruc(periodcounter).Amp(evnum,currN+1) = B_SourceAmp;
+                PeriodStruc(periodcounter).Phase(evnum,currN+1) = B_SourcePhase;
+                
+                PeriodStruc(periodcounter).ratio_AmpMax(evnum,currN+1) = B_SourceAmp./ maxB_SourceAmp;
+
+            end
+        end  
+
+    end
+    % Save excitation structure
+    eventcs.source(ista).excitation = PeriodStruc;
+
+    periodcounter=0;
+    
+    for period = Periodlist
+        periodcounter=periodcounter+1;
+
+        RawExcitation_Mat = PeriodStruc(periodcounter).Amp;
+
+        if MaxN > 0
+            for overtone_num = [1:1:MaxN]
+                ExcitationRatio_Mat(:,overtone_num) = RawExcitation_Mat(:,overtone_num+1)./RawExcitation_Mat(:,1);
             end
         end
-    end  
-    
-    
-    
-end
 
-periodcounter=0;
-
-for period = Periodlist
-    Make_Filename
-    periodcounter=periodcounter+1;
-        
-    RawExcitation_Mat = PeriodStruc(periodcounter).RawExcitation_Mat;
-    
-    for overtone_num = [1:1:MaxN]
-        ExcitationRatio_Mat(:,overtone_num) = RawExcitation_Mat(:,overtone_num+1)./RawExcitation_Mat(:,1);
+        eventcs.source(ista).excitation(periodcounter).ratio_Amp1_0 = ExcitationRatio_Mat;
     end
-    
-    % Output Raw Excitations as text file
-    dlmwrite(RawExcitationFname,PeriodStruc(periodcounter).RawExcitation_Mat,'delimiter','\t','precision','%.25f')
-    
-    if MaxN > 0
-    % Output Excitation Ratios as text file  
-    dlmwrite(ExcitationRatioFname,ExcitationRatio_Mat,'delimiter','\t','precision','%.25f')
-    end
-        clear ExcitationRatio_Mat
-
-    
 end
 
 
