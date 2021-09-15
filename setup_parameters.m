d9c4defba11ad9323e20518b0ace46a51dc99e8a
jbrussell <jbrussell93@gmail.com>
2021-09-15 09:56:01 -0400

    cleanup parameter file

--- setup_parameters.m
+++ setup_parameters.m
@@ -31,7 +31,7 @@ parameters.periods = round(logspace(log10(20),log10(150),15));
 % parameters.eventfile = 'eventlist';
 
 %%%% %%%% %%%% %%%% %%%% %%%% %%%% %%%% %%%% %%%%
-% Parameters for own data selection criteria
+% Parameters for own data selection criteria (if using SAC)
 parameters.dbpath = '/path/to/sac/data/directory/'; 
 parameters.eventfile = 'evlist.txt'; % name of eventlist located in dbpath
 parameters.minMw = 5.5;
@@ -40,33 +40,33 @@ parameters.snr_tol = 3;
 
 %%%% %%%% %%%% %%%% %%%% %%%% %%%% %%%% %%%% %%%%
 % parameters for the auto_win_select.m
-parameters.largest_epidist_range = 3000;
-parameters.cycle_before = 2;
-parameters.cycle_after = 3;
-parameters.min_dist_tol = deg2km(20);
-parameters.max_dist_tol = deg2km(100); %deg2km(160);
-parameters.min_groupv = 2;
-parameters.max_groupv = 5;
+parameters.largest_epidist_range = 3000; % [km] maximum array size to consider
+parameters.cycle_before = 2; % number of cycles to buffer at start of window
+parameters.cycle_after = 3; % number of cycles to buffer at end of window
+parameters.min_dist_tol = deg2km(20); % min epicentral distance
+parameters.max_dist_tol = deg2km(100); %deg2km(160); % min epicentral distance
+parameters.min_groupv = 2; % minimum group velocity as a starting guess
+parameters.max_groupv = 5; % maximum group velocity as a starting guess
 parameters.cent_freq = 0.025;
 parameters.min_sta_num = 5; %10 %JBR
 
 %%%% %%%% %%%% %%%% %%%% %%%% %%%% %%%% %%%% %%%%
 % parameters for the cross-correlation measurement
 % (gsdfmain.m)
-parameters.minstadist = 5;
-parameters.maxstadist = 600; %600; %250 %200;   % station cross-correlation distance in km
-parameters.is_rm_resp = 0;
+parameters.minstadist = 5; % minimum station cross-correlation distance in km
+parameters.maxstadist = 600; %600; %250 %200;   % maximum station cross-correlation distance in km
+parameters.is_rm_resp = 0; % remove instrument response?
 parameters.periods = sort(parameters.periods);  % make sure periods are ascending
-parameters.refv = 4;   % to select the correct cycle
+parameters.refv = 4;   % to select the correct cycle (for cycle skipping correction)
 parameters.refphv = ones(size(parameters.periods))*4;
-parameters.min_width = 0.06;  % to build up gaussian filters
-parameters.max_width = 0.10;  
+parameters.min_width = 0.10; %0.06;  % to build up gaussian filters
+parameters.max_width = parameters.min_width; %0.10;  
 parameters.wintaperlength = 30;   % taper to build up the isolation filter
 parameters.prefilter = [10,190]; %[15,160]; %[10,200];
-parameters.xcor_win_halflength = 200; %300; %200 %150;  % window for the cross-correlation
+parameters.xcor_win_halflength = 200; %300; %200 %150;  % window length for the cross-correlation
 parameters.xcor_win_iter = zeros(size(parameters.periods)); % re-apply the xcor window due to measured group delay, should be same length as periods, not used anymore
-parameters.Nfit = 2; %4; % 2
-parameters.Ncircle = 5;
+parameters.Nfit = 2; %4; % 2 % number of cycles fit on either side of maximum for gaussian wavelet
+parameters.Ncircle = 5; % number of cycles searched for cycle skipping
 parameters.cohere_tol = 0.65; % minimum coherenecy between two stations
 parameters.tp_tol = 10;  % seconds away from averaged phase velocity 
 parameters.is_winx2 = 1; % jbr - 0=window single seismogram (default); 1=window both seismograms (may help reduce overtone interference)
@@ -74,13 +74,13 @@ parameters.is_winx2 = 1; % jbr - 0=window single seismogram (default); 1=window 
 %%%% %%%% %%%% %%%% %%%% %%%% %%%% %%%% %%%% %%%%
 % parameters for the tomography
 % (eikonal_eq.m helmholtz_eq.m)
-parameters.smweight_array = 3*[0.4 0.3 0.2 0.2 0.2 0.5 0.5 0.5 1 1 1 2 3 3 3]; %3*[0.4 0.3 0.2 0.2 0.2 0.5 1 2 2 3 3] %3*[0.4 0.3 0.2 0.2 0.2 0.5 1 2];  % smoothing weight for the deltaSx and delta Sy
-parameters.flweight_array = 0*ones(size(parameters.periods)); % JBR
+parameters.smweight_array = 3*[0.4 0.3 0.2 0.2 0.2 0.5 0.5 0.5 1 1 1 2 3 3 3];  % 2nd derivative smoothing weight for the deltaSx and delta Sy
+parameters.flweight_array = 0*ones(size(parameters.periods)); % JBR - 1st derivative smoothing
 parameters.raydensetol=deg2km(parameters.gridsize)*2;
-parameters.Tdumpweight = 0;  % dumping the ray to the girgle circle path
-parameters.Rdumpweight = 0;  % dumping the region to have the same phase velocity
+parameters.Tdumpweight = 0;  % damping the ray to the great-circle path
+parameters.Rdumpweight = 0;  % damping the region to have the same phase velocity
 parameters.fiterrtol = 3;   % error allowed in the wavelet fitting
-parameters.isRsmooth = 1;  % smoothing due to Sx and Sy or Sr and S_theta
+parameters.isRsmooth = 1;  % smoothing due to Sx and Sy or Sr and S_theta; 1=Sr,S_theta
 parameters.dterrtol = 2;    % largest variance of the inversion error allowed
 parameters.inverse_err_tol = 2;  % count be number of standard devition
 parameters.min_amp_tol = 0.1;  % station with amplitude smaller than this ratio of average amplitude will not be used.
@@ -92,8 +92,8 @@ parameters.alpha_search_grid = 0.1;
 % parameter for stacking 
 % (stack_phv.m stack_helm.m)
 parameters.min_csgoodratio= 1*ones(length(parameters.periods));%[3 3 3 3 5 10 15 15 15 15 20]; %[3 3 3 3 5 10 15 15]; % minimum radio between good and bad measurements for a good event
-parameters.min_phv_tol = 3;
-parameters.max_phv_tol = 5;
+parameters.min_phv_tol = 3; % minimum allowed phase velocity
+parameters.max_phv_tol = 5; % maximum allowed phase velocity
 parameters.is_raydense_weight = 0; %1; % manual says suggested turned off for large azimuthal anisotropy
 parameters.min_event_num = 3; %10;
 parameters.err_std_tol = 2;
