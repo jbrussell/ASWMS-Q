933b26e5a3eed8754f2ed096d0d45a976aca0d2e
jbrussell <jbrussell93@gmail.com>
2021-10-03 19:46:08 -0400

    a0: add a run file

---
+++ a0_run_simple.m
@@ -0,0 +1,23 @@
+% Run ASWMS
+
+a1_a_sac2eventmat
+
+a2_cleanup_events
+
+a3_a_run_autowinpick
+
+a4_gsdfmain
+
+a6_a_eikonal_eq
+
+a7_a_stack_phv
+
+a8_ampgrad_inv
+
+a9_a_helmholtz_eq
+
+a9_b_stack_helm
+
+a11_fit_tp_surf
+
+b1_estimate_alpha_beta_Lin12
