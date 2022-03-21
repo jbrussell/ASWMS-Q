a6c60770b80b88e23b1f18839a4827bb55464a8d
jbrussell <jbrussell93@gmail.com>
2022-03-21 19:52:06 -0400

    slight modification to gradient functions

--- functions/del2m.m
+++ functions/del2m.m
@@ -1,17 +1,21 @@
 function dA=del2m(xi,yi,A)
 
 [m,n]=size(xi);
-dA=nan(m,n);
-
+dAx=nan(m,n);
+dAy=nan(m,n);
 
 for i=2:m-1
-	for j=2:n-1
-		h1=vdist(xi(i-1,j),yi(i-1,j),xi(i+1,j),yi(i+1,j))/1e3/2;
-		dA(i,j)=(A(i+1,j)-2*A(i,j)+A(i-1,j))/h1.^2;
-		h2=vdist(xi(i,j-1),yi(i,j-1),xi(i,j+1),yi(i,j+1))/1e3/2;
-		dA(i,j)=dA(i,j)+(A(i,j+1)-2*A(i,j)+A(i,j-1))/h2.^2;
-	end
+    for j=2:n-1
+        xh1=vdist(xi(i-1,j),yi(i-1,j),xi(i,j),yi(i,j))/1e3;
+        xh2=vdist(xi(i+1,j),yi(i+1,j),xi(i,j),yi(i,j))/1e3;
+        dAx(i,j)=2.*(xh2*A(i-1,j)-(xh1+xh2)*A(i,j)+xh1*A(i+1,j))/(xh1*xh2*(xh1+xh2));
+
+        yh1=vdist(xi(i,j-1),yi(i,j-1),xi(i,j),yi(i,j))/1e3;
+        yh2=vdist(xi(i,j+1),yi(i,j+1),xi(i,j),yi(i,j))/1e3;
+        dAy(i,j)=2.*(yh2*A(i,j-1)-(yh1+yh2)*A(i,j)+yh1*A(i,j+1))/(yh1*yh2*(yh1+yh2));
+    end
 end
+dA = dAx + dAy;
 
 % now fill in the edges
 for i=1:m
--- functions/delm.m
+++ functions/delm.m
@@ -9,12 +9,15 @@ dAlat=nan(m,n);
 dAlon=nan(m,n);
 
 for i=2:m-1
-	for j=2:n-1
-		h1=vdist(xi(i-1,j),yi(i-1,j),xi(i+1,j),yi(i+1,j))/1e3/2;
-		dAlat(i,j)=(A(i+1,j)-A(i-1,j))/h1/2;
-		h2=vdist(xi(i,j-1),yi(i,j-1),xi(i,j+1),yi(i,j+1))/1e3/2;
-		dAlon(i,j)=(A(i,j+1)-A(i,j-1))/h2/2;
-	end
+    for j=2:n-1
+        xh1=vdist(xi(i-1,j),yi(i-1,j),xi(i,j),yi(i,j))/1e3;
+        xh2=vdist(xi(i+1,j),yi(i+1,j),xi(i,j),yi(i,j))/1e3;
+        dAlat(i,j)=-A(i-1,j)*xh2/(xh1*(xh1+xh2)) - A(i,j)*(xh1-xh2)/(xh1*xh2) + A(i+1,j)*xh1/(xh2*(xh1+xh2));
+
+        yh1=vdist(xi(i,j-1),yi(i,j-1),xi(i,j),yi(i,j))/1e3;
+        yh2=vdist(xi(i,j+1),yi(i,j+1),xi(i,j),yi(i,j))/1e3;
+        dAlon(i,j)=-A(i,j-1)*yh2/(yh1*(yh1+yh2)) - A(i,j)*(yh1-yh2)/(yh1*yh2) + A(i,j+1)*yh1/(yh2*(yh1+yh2));
+    end
 end
 dA = (dAlon.^2 + dAlat.^2).^0.5;
 
