f7137ef9c7f7bf81d7695be163a656556f3fd4e9
jbrussell <jbrussell93@gmail.com>
2022-03-21 12:09:47 -0400

    more correctly account for latitude in smoothing kernels

--- functions/smooth_kernel_build.m
+++ functions/smooth_kernel_build.m
@@ -13,17 +13,21 @@ Ny = length(ynode);
 % Smoothing in y
 [i,j] = ndgrid(1:Nx,2:(Ny-1));
 ind = j(:) + Ny*(i(:)-1);
-dy = diff(ynode)*cosd(mean(xnode));  % correct smoothing for latitude;
-dy1 = dy(j(:)-1);
-dy2 = dy(j(:));
+% dy = diff(ynode)*cosd(mean(xnode));  % correct smoothing for latitude;
+% dy1 = dy(j(:)-1);
+% dy2 = dy(j(:));
+dy1 = km2deg(distance(xnode(i(:)),ynode(j(:)),xnode(i(:)),ynode(j(:)-1),referenceEllipsoid('GRS80'))/1000);
+dy2 = km2deg(distance(xnode(i(:)),ynode(j(:)),xnode(i(:)),ynode(j(:)+1),referenceEllipsoid('GRS80'))/1000);
 Areg = sparse(repmat(ind,1,3),[ind-1,ind,ind+1], ...
     [-2./(dy1.*(dy1+dy2)), 2./(dy1.*dy2), -2./(dy2.*(dy1+dy2))],N,N);
 % Smoothing in x
 [i,j] = ndgrid(2:(Nx-1),1:Ny);
 ind = j(:) + Ny*(i(:)-1);
-dx = diff(xnode);
-dx1 = dx(i(:)-1);
-dx2 = dx(i(:));
+% dx = diff(xnode);
+% dx1 = dx(i(:)-1);
+% dx2 = dx(i(:));
+dx1 = km2deg(distance(xnode(i(:)),ynode(j(:)),xnode(i(:)-1),ynode(j(:)),referenceEllipsoid('GRS80'))/1000);
+dx2 = km2deg(distance(xnode(i(:)),ynode(j(:)),xnode(i(:)+1),ynode(j(:)),referenceEllipsoid('GRS80'))/1000);
 Areg = [Areg;sparse(repmat(ind,1,3),[ind-Ny,ind,ind+Ny], ...
     [-2./(dx1.*(dx1+dx2)), 2./(dx1.*dx2), -2./(dx2.*(dx1+dx2))],N,N)];
 F=Areg;
