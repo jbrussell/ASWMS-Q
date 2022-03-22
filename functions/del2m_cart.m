e1ac6ec3d31a8edf32d2f5f992b302b7abd80208
jbrussell <jbrussell93@gmail.com>
2022-03-22 10:41:18 -0400

    small update

--- functions/del2m_cart.m
+++ functions/del2m_cart.m
@@ -7,11 +7,11 @@ dAy=nan(m,n);
 for i=2:m-1
 	for j=2:n-1
 		xh1=sqrt((xi(i-1,j)-xi(i,j)).^2 + (yi(i-1,j)-yi(i,j)).^2);
-        xh2=sqrt((xi(i+1,j)-xi(i,j)).^2 + (yi(i+1,j)-yi(i,j)).^2);
+	    xh2=sqrt((xi(i+1,j)-xi(i,j)).^2 + (yi(i+1,j)-yi(i,j)).^2);
 		dAx(i,j)=2.*(xh2*A(i-1,j)-(xh1+xh2)*A(i,j)+xh1*A(i+1,j))/(xh1*xh2*(xh1+xh2));
-        
-        yh1=sqrt((xi(i,j-1)-xi(i,j)).^2 + (yi(i,j-1)-yi(i,j)).^2);
-        yh2=sqrt((xi(i,j+1)-xi(i,j)).^2 + (yi(i,j+1)-yi(i,j)).^2);
+	    
+	    yh1=sqrt((xi(i,j-1)-xi(i,j)).^2 + (yi(i,j-1)-yi(i,j)).^2);
+	    yh2=sqrt((xi(i,j+1)-xi(i,j)).^2 + (yi(i,j+1)-yi(i,j)).^2);
 		dAy(i,j)=2.*(yh2*A(i,j-1)-(yh1+yh2)*A(i,j)+yh1*A(i,j+1))/(yh1*yh2*(yh1+yh2));
 	end
 end
