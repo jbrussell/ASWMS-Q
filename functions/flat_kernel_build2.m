6e61523110bc3a22692a79e4310e367aa6ea0561
jbrussell <jbrussell93@gmail.com>
2022-03-22 09:45:18 -0400

    1st deriv: use 3 pt kernel rather than 2 pt

--- functions/flat_kernel_build2.m
+++ functions/flat_kernel_build2.m
@@ -1,5 +1,5 @@
 function F = flat_kernel_build2(xnode, ynode, N)
-% Build flattening kernel for tomography problem
+% Build flattening kernel for tomography problem using 3 points
 % (i.e., smooth first derivative)
 % 
 % N is the total number of model parameters in the final G matrix
@@ -12,21 +12,23 @@ Nx = length(xnode);
 Ny = length(ynode);
 
 % Smoothing in y
-[i,j] = ndgrid(1:Nx,1:(Ny-1));
+[i,j] = ndgrid(1:Nx,2:(Ny-1));
 ind = j(:) + Ny*(i(:)-1);
 % dy = diff(ynode);
 % dy2 = dy(j(:));
-dy2 = km2deg(distance(xnode(i),ynode(j),xnode(i),ynode(j+1),referenceEllipsoid('GRS80'))/1000);
-Areg = sparse(repmat(ind,1,2),[ind,ind+1], ...
-    [-1./dy2, 1./dy2],N,N);
+dy1 = km2deg(distance(xnode(i(:)),ynode(j(:)),xnode(i(:)),ynode(j(:)-1),referenceEllipsoid('GRS80'))/1000);
+dy2 = km2deg(distance(xnode(i(:)),ynode(j(:)),xnode(i(:)),ynode(j(:)+1),referenceEllipsoid('GRS80'))/1000);
+Areg = sparse(repmat(ind,1,3),[ind-1,ind,ind+1], ...
+    [-dy2./(dy1.*(dy1+dy2)), -(dy1-dy2)./(dy1.*dy2), dy1./(dy2.*(dy1+dy2))],N,N);
 % Smoothing in x
-[i,j] = ndgrid(1:(Nx-1),1:Ny);
+[i,j] = ndgrid(2:(Nx-1),1:Ny);
 ind = j(:) + Ny*(i(:)-1);
 % dx = diff(xnode);
 % dx2 = dx(i(:));
-dx2 = km2deg(distance(xnode(i),ynode(j),xnode(i+1),ynode(j),referenceEllipsoid('GRS80'))/1000);
-Areg = [Areg;sparse(repmat(ind,1,2),[ind,ind+Ny], ...
-    [-1./dx2, 1./dx2],N,N)];
+dx1 = km2deg(distance(xnode(i(:)),ynode(j(:)),xnode(i(:)-1),ynode(j(:)),referenceEllipsoid('GRS80'))/1000);
+dx2 = km2deg(distance(xnode(i(:)),ynode(j(:)),xnode(i(:)+1),ynode(j(:)),referenceEllipsoid('GRS80'))/1000);
+Areg = [Areg;sparse(repmat(ind,1,3),[ind-Ny,ind,ind+Ny], ...
+    [-dx2./(dx1.*(dx1+dx2)), -(dx1-dx2)./(dx1.*dx2), dx1./(dx2.*(dx1+dx2))],N,N)];
 F=Areg;
 
 if 0
