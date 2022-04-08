c2def5af07c44404deaa5a6f031e44374a9c91b1
jbrussell <jbrussell93@gmail.com>
2022-04-07 23:05:32 -0400

    TPW: update function for reading station correction terms
    to be more robust

--- TPW/functions/load_stacorfile.m
+++ TPW/functions/load_stacorfile.m
@@ -1,12 +1,18 @@
 function [mat] = load_stacorfile(file,stamat)
 %Load alpha file and interpolate to desired grid
 %
-data = readtable(file);
-
-sta = data.Var4;
-stacor = data.Var2;
-stacor_std = data.Var3;
-staid = data.Var1;
+fid = fopen(file);
+ista = 0;
+while ~feof(fid)
+    ista = ista + 1;
+    tline = fgetl(fid);
+    scan = textscan(tline,'%f %f %f %s');
+    staid(ista) = scan{1};
+    stacor(ista)  = scan{2};
+    stacor_std(ista)  = scan{3};
+    sta{ista}  = scan{4}{1};
+end
+fclose(fid);
 
 stas = load(stamat);
 [~,I] = intersect(stas.stnms,sta);
