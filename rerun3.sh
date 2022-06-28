a1b6f16040f80457859e9edfd4f3b0a785f03300
Xinxuan Lu <46036791+Egret-Lu@users.noreply.github.com>
2022-06-28 14:41:49 -0400

    add one file on github

---
+++ rerun3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd /scratch/tolugboj_lab/Prj6_AfrTomography/7_AFANT/YT_runs_Jan2022/L25M3_110K
+/scratch/tolugboj_lab/Prj6_AfrTomography/7_AFANT/for_Tolu/tomo_dependency/AFANT_R40_Meth3/prepare_n_submit.sh 12 3
+
+cd /scratch/tolugboj_lab/Prj6_AfrTomography/7_AFANT/YT_runs_Jan2022/L30M3_110K
+/scratch/tolugboj_lab/Prj6_AfrTomography/7_AFANT/for_Tolu/tomo_dependency/AFANT_R40_Meth3/prepare_n_submit.sh 15 8
+
+cd /scratch/tolugboj_lab/Prj6_AfrTomography/7_AFANT/YT_runs_Jan2022/L35M3_110K
+/scratch/tolugboj_lab/Prj6_AfrTomography/7_AFANT/for_Tolu/tomo_dependency/AFANT_R40_Meth3/prepare_n_submit.sh 12 12
+
+cd /scratch/tolugboj_lab/Prj6_AfrTomography/7_AFANT/YT_runs_Jan2022/L40M3_110K_GRP
+/scratch/tolugboj_lab/Prj6_AfrTomography/7_AFANT/for_Tolu/tomo_dependency/AFANT_R40_Meth3/prepare_n_submit.sh 15 13
+
+cd /scratch/tolugboj_lab/Prj6_AfrTomography/7_AFANT/YT_runs_Jan2022/L40M3_110K
+/scratch/tolugboj_lab/Prj6_AfrTomography/7_AFANT/for_Tolu/tomo_dependency/AFANT_R40_Meth3/prepare_n_submit.sh 15 3
+
